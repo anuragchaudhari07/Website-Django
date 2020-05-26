112456db9ef44e46c38c07b020e7897fb8eda09b
Anurag Chaudhari <54589221+anuragchaudhari07@users.noreply.github.com>
2020-05-26 17:39:21 +0530

    updated models.py

--- ebarristers/domestic/models.py
+++ ebarristers/domestic/models.py
@@ -21,7 +21,7 @@ ICC_CHOICES = (
 
 class DomesticComplaint(models.Model):
 
-    doc_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,verbose_name='Complainant Name')  #pk
+    doc_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,verbose_name='Complaint ID')  #pk
 
     # own info of customer
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete = models.CASCADE,null=True)
